fbe820bcc1398a6aefcc5e56b461e1082dfc884c
bhagwanabaird720-droid <bhagwanabaird720@gmail.com>
2026-08-14 18:25:17 +0530

    Add transition timings for motion effects

--- packages/design-system/motion/transitions.ts
+++ packages/design-system/motion/transitions.ts
@@ -1,1 +1,6 @@
-
+export const transitions = {
+  fast: '120ms ease-out',
+  normal: '200ms ease-out',
+  slow: '320ms ease-out',
+  sidebarToggle: '250ms cubic-bezier(0.4, 0, 0.2, 1)',
+} as const;
