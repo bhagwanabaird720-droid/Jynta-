84a603b9cebf5c72506d7034724536aa7dcc1bd4
bhagwanabaird720-droid <bhagwanabaird720@gmail.com>
2026-08-16 13:58:36 +0530

    Wrap ComingSoon component in AppShell

--- apps/web/src/app/assistant/learn/3d/page.tsx
+++ apps/web/src/app/assistant/learn/3d/page.tsx
@@ -1,4 +1,10 @@
+import { AppShell } from '@/components/layout/AppShell';
 import { ComingSoon } from '@/components/common/ComingSoon';
+
 export default function Page() {
-  return <ComingSoon title="3D Learning" />;
+  return (
+    <AppShell>
+      <ComingSoon title="3D Learning" />
+    </AppShell>
+  );
 }
