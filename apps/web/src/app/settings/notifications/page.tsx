7bf7744845f31e88e46b5f67c162a0597d6e0c55
bhagwanabaird720-droid <bhagwanabaird720@gmail.com>
2026-08-16 13:54:53 +0530

    Wrap ComingSoon component in AppShell

--- apps/web/src/app/settings/notifications/page.tsx
+++ apps/web/src/app/settings/notifications/page.tsx
@@ -1,4 +1,10 @@
+import { AppShell } from '@/components/layout/AppShell';
 import { ComingSoon } from '@/components/common/ComingSoon';
+
 export default function Page() {
-  return <ComingSoon title="Notification Settings" />;
+  return (
+    <AppShell>
+      <ComingSoon title="Notification Settings" />
+    </AppShell>
+  );
 }
