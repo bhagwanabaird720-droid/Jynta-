68d981bf2eaff49e0c35a0874daf07b966972e75
bhagwanabaird720-droid <bhagwanabaird720@gmail.com>
2026-08-16 13:08:32 +0530

    Wrap ComingSoon component in AppShell

--- apps/web/src/app/memory/page.tsx
+++ apps/web/src/app/memory/page.tsx
@@ -1,4 +1,10 @@
+import { AppShell } from '@/components/layout/AppShell';
 import { ComingSoon } from '@/components/common/ComingSoon';
+
 export default function Page() {
-  return <ComingSoon title="Memory" />;
+  return (
+    <AppShell>
+      <ComingSoon title="Memory" />
+    </AppShell>
+  );
 }
