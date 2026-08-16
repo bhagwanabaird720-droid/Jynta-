456322cd0bde10605d1639f8b14a1526e34362e6
bhagwanabaird720-droid <bhagwanabaird720@gmail.com>
2026-08-16 13:54:15 +0530

    Wrap ComingSoon component in AppShell

--- apps/web/src/app/settings/language/page.tsx
+++ apps/web/src/app/settings/language/page.tsx
@@ -1,4 +1,10 @@
+import { AppShell } from '@/components/layout/AppShell';
 import { ComingSoon } from '@/components/common/ComingSoon';
+
 export default function Page() {
-  return <ComingSoon title="Language" />;
+  return (
+    <AppShell>
+      <ComingSoon title="Language" />
+    </AppShell>
+  );
 }
