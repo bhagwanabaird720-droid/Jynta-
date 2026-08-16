483d1deddf027e725e2e2ccf70cc837b5e4d2995
bhagwanabaird720-droid <bhagwanabaird720@gmail.com>
2026-08-16 14:02:06 +0530

    Wrap ComingSoon component in AppShell

--- apps/web/src/app/automation/tasks/[taskId]/page.tsx
+++ apps/web/src/app/automation/tasks/[taskId]/page.tsx
@@ -1,4 +1,10 @@
+import { AppShell } from '@/components/layout/AppShell';
 import { ComingSoon } from '@/components/common/ComingSoon';
+
 export default function Page() {
-  return <ComingSoon title="Task Details" />;
+  return (
+    <AppShell>
+      <ComingSoon title="Task Details" />
+    </AppShell>
+  );
 }
