8bebd515ca49f8f89730a1e9368748b5dab90aae
bhagwanabaird720-droid <bhagwanabaird720@gmail.com>
2026-08-16 13:46:11 +0530

    Wrap ComingSoon component in AppShell

--- apps/web/src/app/memory/videos/page.tsx
+++ apps/web/src/app/memory/videos/page.tsx
@@ -1,4 +1,10 @@
+import { AppShell } from '@/components/layout/AppShell';
 import { ComingSoon } from '@/components/common/ComingSoon';
+
 export default function Page() {
-  return <ComingSoon title="Videos" />;
+  return (
+    <AppShell>
+      <ComingSoon title="Videos" />
+    </AppShell>
+  );
 }
