7841786bd90f91f08e3e0434affe2cda58139d76
bhagwanabaird720-droid <bhagwanabaird720@gmail.com>
2026-08-16 13:27:00 +0530

    Wrap ComingSoon component in AppShell

--- apps/web/src/app/profile/edit/page.tsx
+++ apps/web/src/app/profile/edit/page.tsx
@@ -1,4 +1,10 @@
+import { AppShell } from '@/components/layout/AppShell';
 import { ComingSoon } from '@/components/common/ComingSoon';
+
 export default function Page() {
-  return <ComingSoon title="Edit Profile" />;
+  return (
+    <AppShell>
+      <ComingSoon title="Edit Profile" />
+    </AppShell>
+  );
 }
